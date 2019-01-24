6c4b7bcadcbe115bbb9f737303232fc140327eea
stefan <383411934@qq.com>
2019-01-24 15:22:47 +0800

    首页banner接口数据设置

--- app/src/main/java/com/example/administrator/ktplayandroid/main_fragment/HomeFragment.kt
+++ app/src/main/java/com/example/administrator/ktplayandroid/main_fragment/HomeFragment.kt
@@ -6,17 +6,27 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
+import com.chad.library.adapter.base.BaseQuickAdapter
 import com.example.administrator.ktplayandroid.R
 import com.example.administrator.ktplayandroid.adapter.HomeAdapter
+import com.example.administrator.ktplayandroid.view.GlideImageLoader
+import com.example.administrator.ktplayandroid.view.WebViewActivity
 import com.example.handsomelibrary.api.ApiService
 import com.example.handsomelibrary.base.BaseFragment
 import com.example.handsomelibrary.interceptor.Transformer
 import com.example.handsomelibrary.model.ArticleListBean
+import com.example.handsomelibrary.model.BannerBean
 import com.example.handsomelibrary.retrofit.RxHttpUtils
 import com.example.handsomelibrary.retrofit.observer.CommonObserver
 import com.example.handsomelibrary.utils.L
 import com.scwang.smartrefresh.layout.api.RefreshLayout
+import com.scwang.smartrefresh.layout.constant.RefreshState
 import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.home_banner.*
+import com.youth.banner.BannerConfig
+import com.youth.banner.Transformer.DepthPage
+import com.youth.banner.listener.OnBannerListener
 
 /**
  * 首页
@@ -24,7 +34,8 @@ import com.scwang.smartrefresh.layout.listener.OnRefreshLoadMoreListener
  */
 
 class HomeFragment : BaseFragment(), OnRefreshLoadMoreListener {
-     private lateinit var mAdapter: HomeAdapter
+    private lateinit var mAdapter: HomeAdapter
+    private var mPageNo: Int = 0
 
     companion object {
         private const val HOME_FRAGMENT = "home"
@@ -43,44 +54,125 @@ class HomeFragment : BaseFragment(), OnRefreshLoadMoreListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView = view.findViewById<RecyclerView>(R.id.rv_main)
-        recyclerView.layoutManager = LinearLayoutManager(mContext)
+        recyclerView.layoutManager = LinearLayoutManager(mContext) as RecyclerView.LayoutManager?
         mAdapter = HomeAdapter(ArrayList())
         recyclerView.adapter = mAdapter
+        refreshLayout.setOnRefreshLoadMoreListener(this)//监听刷新
         //添加头部
         val inflate = LayoutInflater.from(mContext).inflate(R.layout.home_banner, null)
         mAdapter.addHeaderView(inflate)
+        mAdapter.onItemClickListener =
+                BaseQuickAdapter.OnItemClickListener { _, _, position ->
+                    val data = mAdapter.data
+                    if (data.size != 0 && data.isNotEmpty()) {
+                        WebViewActivity.startWebActivity(mContext, data[position].link)
+                    }
+                }
     }
 
     override fun initData() {
-        getArticleListBean()
-       // homeRecycleView = Companion.view?.findViewById(R.id.homeRecycleView) as RecyclerView;
+        getBanner()
+        getArticleListBean(mPageNo)
+        // homeRecycleView = Companion.view?.findViewById(R.id.homeRecycleView) as RecyclerView;
+    }
+
+    //头部banner设置
+    private fun getBanner() {
+        RxHttpUtils.createApi(ApiService::class.java)
+            .banner
+            .compose(Transformer.switchSchedulers())
+            .subscribe(object : CommonObserver<List<BannerBean>>() {
+                override fun onSuccess(t: List<BannerBean>) {
+                    setBannerView(t)
+                }
+
+                override fun onError(errorMsg: String?) {
+
+                }
+            })
+    }
+
+    private fun setBannerView(bannerBean: List<BannerBean>) {
+        val titleList = ArrayList<String>()
+        for (it in 0 until bannerBean.size) {
+            titleList.add(bannerBean[it].title)
+        }
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE)
+        //设置图片加载器
+        banner.setImageLoader(GlideImageLoader())
+        //设置图片集合
+        banner.setImages(bannerBean)
+        //设置banner动画效果
+        // banner.setBannerAnimation(Transformer.DepthPage)
+        //设置标题集合（当banner样式有显示title时）
+        banner.setBannerTitles(titleList)
+        //设置自动轮播，默认为true
+        banner.isAutoPlay(true)
+        //设置轮播时间
+        banner.setDelayTime(1500)
+        //设置指示器位置（当banner模式中有指示器时）
+        banner.setIndicatorGravity(BannerConfig.RIGHT)
+        //点击事件
+        banner.setOnBannerListener { position ->
+            WebViewActivity.startWebActivity(mContext,bannerBean[position].url)
+        }
+        //banner设置方法全部调用完毕时最后调用
+        banner.start()
     }
 
     //获取文章列表
-    private fun getArticleListBean() {
+    private fun getArticleListBean(mPageNo: Int) {
         RxHttpUtils.createApi(ApiService::class.java)
-            .getArticleList(0)
+            .getArticleList(mPageNo)
             .compose(Transformer.switchSchedulers())
-            .subscribe(object : CommonObserver<ArticleListBean>(){
+            .subscribe(object : CommonObserver<ArticleListBean>() {
                 override fun onSuccess(t: ArticleListBean) {
-                    if (t.datas!!.isNotEmpty()){
-                        mAdapter.setNewData(t.datas)
+                    if (t.datas!!.isNotEmpty()) {
+                        when {
+                            refreshLayout.state == RefreshState.Refreshing -> {
+                                mAdapter.setNewData(t.datas)
+                                refreshLayout.finishRefresh()
+                            }//正在加载
+                            refreshLayout.state == RefreshState.Loading -> {
+                                mAdapter.data.addAll(t.datas)
+                                refreshLayout.finishLoadMore()
+                                mAdapter.notifyDataSetChanged()
+                            }
+                            else -> mAdapter.setNewData(t.datas)
+                        }
+                    } else {
+                        if (refreshLayout.state == RefreshState.Refreshing) {
+                            refreshLayout.finishRefresh()
+                        } else if (refreshLayout.state == RefreshState.Loading) {
+                            refreshLayout.finishLoadMore()
+                        }
                     }
                 }
 
                 override fun onError(errorMsg: String?) {
                     L.i(errorMsg)
                 }
-
             })
-
     }
 
     override fun onLoadMore(refreshLayout: RefreshLayout) {
+        mPageNo += 1
+        getArticleListBean(mPageNo)
     }
 
     override fun onRefresh(refreshLayout: RefreshLayout) {
+        mPageNo = 0
+        getArticleListBean(mPageNo)
     }
 
+    override fun onPause() {
+        super.onPause()
+        if (refreshLayout.state == RefreshState.Refreshing) {
+            refreshLayout.finishRefresh()
+        }
+        if (refreshLayout.state == RefreshState.Loading) {
+            refreshLayout.finishLoadMore()
+        }
+    }
 }
 
---
+++ app/src/main/java/com/example/administrator/ktplayandroid/view/WebViewActivity.java
@@ -0,0 +1,165 @@
+package com.example.administrator.ktplayandroid.view;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.http.SslError;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.*;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.OnClick;
+import com.example.administrator.ktplayandroid.R;
+import com.example.handsomelibrary.base.BaseActivity;
+import com.example.handsomelibrary.utils.JumpUtils;
+
+/**
+ * WebView 设置相关
+ * Created by Stefan on 2018/11/26 16:45
+ */
+
+public class WebViewActivity extends BaseActivity {
+
+    @BindView(R.id.webView)
+    WebView webView;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
+    @BindView(R.id.showError)
+    TextView showError;
+    @BindView(R.id.tv_title)
+    TextView tv_title;
+
+    private static final String WEB_URL = "url";
+
+    @Override
+    protected int getContentView() {
+        return R.layout.activity_webview;
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+        initView();
+    }
+
+    @OnClick({R.id.showError, R.id.iv_back})
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.showError:
+                webView.setVisibility(View.VISIBLE);
+                showError.setVisibility(View.GONE);
+                webView.reload();
+                break;
+            case R.id.iv_back:
+                JumpUtils.exitActivity(mContext);
+                break;
+        }
+    }
+
+    public static void startWebActivity(Context context, String url) {
+        Intent intent = new Intent(context, WebViewActivity.class);
+        intent.putExtra(WEB_URL, url);
+        context.startActivity(intent);
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void initView() {
+        String url = getIntent().getStringExtra(WEB_URL);
+        //加快HTML网页加载完成的速度，等页面finish再加载图片
+        if (Build.VERSION.SDK_INT >= 19) {
+            webView.getSettings().setLoadsImagesAutomatically(true);
+        } else {
+            webView.getSettings().setLoadsImagesAutomatically(false);
+        }
+        webView.setHorizontalScrollBarEnabled(false);//水平不显示
+        webView.setVerticalScrollBarEnabled(false); //垂直不显示
+        webView.loadUrl(url);
+        // Enable Javascript
+        WebSettings webSettings = webView.getSettings();
+        //设置WebView支持javascript脚本
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setSupportZoom(true);          //允许缩放
+        webSettings.setBuiltInZoomControls(true);  //原网页基础上缩放
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        // 5.0 以后的WebView加载的链接为Https开头，但是链接里面的内容，比如图片为Http链接，加载图片方法
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            //两者都可以
+            webSettings.setMixedContentMode(webSettings.getMixedContentMode());
+            //webView.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+        }
+        // Force links and redirects to open in the WebView instead of in a browser
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            //用网页的标题来设置自己的标题栏
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                if (null != title) {
+                    tv_title.setText(title);
+                } else {
+                    tv_title.setText("");
+                }
+            }
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress == 100) {
+                    progressBar.setVisibility(View.GONE);//加载完网页进度条消失
+                } else {
+                    progressBar.setVisibility(View.VISIBLE);//开始加载网页时显示进度条
+                    progressBar.setProgress(newProgress);//设置进度值
+                }
+            }
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                webView.setVisibility(View.GONE);
+               // showError.setVisibility(View.VISIBLE);
+                super.onReceivedError(view, request, error);
+            }
+
+            //在认证证书不被Android所接受的情况下，我们可以通过设置重写WebViewClient的onReceivedSslError方法在其中设置接受所有网站的证书来解决
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                //super.onReceivedSslError(view, handler, error);注意一定要去除这行代码，否则设置无效。
+                // handler.cancel();// Android默认的处理方式
+                handler.proceed();// 接受所有网站的证书
+                // handleMessage(Message msg);// 进行其他处理
+            }
+        });
+    }
+
+    //销毁WebView
+    @Override
+    public void onDestroy() {
+        if (webView != null) {
+            webView.loadDataWithBaseURL(null, "", "text/html", "utf-8", null);
+            webView.removeAllViews();
+            //在5.1上如果不加上这句话就会出现内存泄露。这是5.1的bug
+            // mComponentCallbacks导致的内存泄漏
+            webView.setTag(null);
+            webView.clearHistory();
+            ((ViewGroup) webView.getParent()).removeView(webView);
+            webView.destroy();
+            webView = null;
+        }
+        super.onDestroy();
+    }
+
+    //改写物理按键——返回的逻辑
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()) {
+            webView.goBack();//返回上一页面
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);//退出整个应用程序
+    }
+}
